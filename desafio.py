687d7c1e0b7bd29258ff90389da026d840a122ec
joao toledo <jvmstoledo@gmail.com>
2026-03-31 21:24:32 -0300

    a

--- desafio.py
+++ desafio.py
@@ -26,7 +26,7 @@ p25 = calculo_percentil(lista, 25)
 p50 = calculo_percentil(lista, 50)
 p75 = calculo_percentil(lista, 75)
 
-print("\n📊 Resultados dos Percentis:")
+print(" Resultados dos Percentis:")
 print(f"P25: {p25}")
 print(f"P50 (Mediana): {p50}")
 print(f"P75: {p75}")
